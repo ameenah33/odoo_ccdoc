606f30a75ae714dec6e395098cb0711c8b20393c
Aminata <aminatadiagne@MacBook-Pro-de-Aminata.local>
2026-01-30 15:52:29 +0000

    Ajout de Taxes pré-configurées

--- custom_addons/ccdoc_custom/__manifest__.py
+++ custom_addons/ccdoc_custom/__manifest__.py
@@ -9,9 +9,10 @@
         - Ajoute des champs personnalisés aux Projets
     """,
     'author': 'CCDOC',
-    'depends': ['crm', 'project', 'sale', 'mail', 'purchase'],
+    'depends': ['crm', 'project', 'sale', 'mail', 'purchase', 'account'],
     'data': [
         'security/ir.model.access.csv',
+        'data/account_tax_data.xml',
         'wizard/crm_lead_justify_win_wizard_view.xml',
         'wizard/sale_order_import_wizard_view.xml',
         'views/crm_lead_views.xml',
