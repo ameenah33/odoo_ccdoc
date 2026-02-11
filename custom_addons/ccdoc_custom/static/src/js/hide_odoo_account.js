7338a01a67a79ad73fb0c581b769472ecfab9b88
Aminata <aminatadiagne@MacBook-Pro-de-Aminata.local>
2026-02-11 09:55:11 +0100

    Fix: patch OWL avec patchName pour Odoo 17

--- custom_addons/ccdoc_custom/static/src/js/hide_odoo_account.js
+++ custom_addons/ccdoc_custom/static/src/js/hide_odoo_account.js
@@ -3,9 +3,9 @@
 import { patch } from "@web/core/utils/patch";
 import { UserMenu } from "@web/webclient/user_menu/user_menu";
 
-patch(UserMenu.prototype, {
+patch(UserMenu.prototype, "ccdoc_custom.hide_odoo_account", {
     getElements() {
-        const items = super.getElements(...arguments);
+        const items = this._super(...arguments);
         // Filtrer pour retirer "Mon compte Odoo.com" (odoo_account)
         return items.filter((item) => item.id !== "odoo_account");
     },
